4a88c81f4e82b163eee26905795104d5689bec48
Odd Alexander Tellefsen <oatell03@uia.no>
2021-02-23 20:46:04 +0100

    Added branch with half notes, and fixed bugs

--- SuperPiano/app/src/main/java/com/example/superpiano/HalfTonePianoKeyFragment.kt
+++ SuperPiano/app/src/main/java/com/example/superpiano/HalfTonePianoKeyFragment.kt
@@ -6,18 +6,20 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import com.example.superpiano.databinding.FragmentFullTonePianoKeyBinding
-import kotlinx.android.synthetic.main.fragment_full_tone_piano_key.view.*
+import android.widget.Button
+import com.example.superpiano.databinding.FragmentHalfTonePianoKeyBinding
+import kotlinx.android.synthetic.main.fragment_half_tone_piano_key.view.*
 
 
-class FullTonePianoKeyFragment : Fragment() {
+class HalfTonePianoKeyFragment : Fragment() {
 
-    private var _binding:FragmentFullTonePianoKeyBinding? = null
+    private var _binding:FragmentHalfTonePianoKeyBinding? = null
     private val binding get() = _binding!!
     private lateinit var note:String
 
     var onKeyDown:((note:String) -> Unit)? = null
     var onKeyUp:((note:String) -> Unit)? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,14 +33,14 @@ class FullTonePianoKeyFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFullTonePianoKeyBinding.inflate(layoutInflater)
+        _binding = FragmentHalfTonePianoKeyBinding.inflate(layoutInflater)
         val view = binding.root
 
-        view.fullToneKeyBtn.setOnTouchListener(object: View.OnTouchListener{
+        view.halfToneKeyBtn.setOnTouchListener(object: View.OnTouchListener{
             override fun onTouch(v: View?, event: MotionEvent?): Boolean{
                 when(event?.action){
-                    MotionEvent.ACTION_DOWN -> this@FullTonePianoKeyFragment.onKeyDown?.invoke(note)
-                    MotionEvent.ACTION_UP -> this@FullTonePianoKeyFragment.onKeyUp?.invoke(note)
+                    MotionEvent.ACTION_DOWN -> this@HalfTonePianoKeyFragment.onKeyDown?.invoke(note)
+                    MotionEvent.ACTION_UP -> this@HalfTonePianoKeyFragment.onKeyUp?.invoke(note)
                 }
                 return true
             }
@@ -48,7 +50,7 @@ class FullTonePianoKeyFragment : Fragment() {
 
     companion object {
         fun newInstance(note:String) =
-                FullTonePianoKeyFragment().apply {
+                HalfTonePianoKeyFragment().apply {
                     arguments = Bundle().apply {
                         putString("NOTE", note)
                     }
--- SuperPiano/app/src/main/java/com/example/superpiano/PianoLayout.kt
+++ SuperPiano/app/src/main/java/com/example/superpiano/PianoLayout.kt
@@ -1,21 +1,25 @@
 package com.example.superpiano
 
+import android.graphics.Color.RED
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.view.isVisible
 import com.example.superpiano.databinding.FragmentPianoBinding
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_full_tone_piano_key.view.*
+import kotlinx.android.synthetic.main.fragment_half_tone_piano_key.*
+import kotlinx.android.synthetic.main.fragment_half_tone_piano_key.view.*
 import kotlinx.android.synthetic.main.fragment_piano.view.*
+import kotlinx.android.synthetic.main.fragment_piano.view.HalfTonePianoKey
 
 
 class PianoLayout : Fragment() {
     private var _binding:FragmentPianoBinding? = null
     private val binding get() = _binding!!
     private val fullTones = listOf("C", "D", "E", "F", "G", "A", "B","C2", "D2", "E2", "F2", "G2")
+    private val halfTones = listOf("C#","D#", "Dummy", "F#","G#","A#", "Dummy", "C2#","D2#","F2#","G2#")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,20 +39,36 @@ class PianoLayout : Fragment() {
 
         fullTones.forEach{
             val fullTonePianoKey = FullTonePianoKeyFragment.newInstance(it)
-
             fullTonePianoKey.onKeyDown =
                 {
-
                     println("Piano key down$it")
                 }
 
             fullTonePianoKey.onKeyUp =
                 {
+                    println("Piano key Up$it")
+                }
+            ft.add(view.FullTonePianoKey.id, fullTonePianoKey, "note_$it")
+        }
 
+        halfTones.forEach{
+            val halfTonePianoKey = HalfTonePianoKeyFragment.newInstance(it)
+            halfTonePianoKey.onKeyDown =
+                {
+                    println("Piano key down$it")
+                }
+
+            halfTonePianoKey.onKeyUp =
+                {
                     println("Piano key Up$it")
                 }
 
-            ft.add(view.pianoKeys.id, fullTonePianoKey, "note_$it")
+            if (it.equals("Dummy"))
+            {
+                //Add Code that adds fragment without button or removes/disables button from existing fragment
+                
+            }
+                ft.add(view.HalfTonePianoKey.id, halfTonePianoKey, "note_$it")
         }
         ft.commit()
         return view
