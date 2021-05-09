235ca57b1f905b6224490f1308b3e7f183553a7c
Odd Alexander Tellefsen <oatell03@uia.no>
2021-05-09 02:25:02 +0200

    Missing victory check

--- knotsandcrosses-main/app/src/main/java/no/uia/ikt205/thgame/api/GameService.kt
+++ knotsandcrosses-main/app/src/main/java/no/uia/ikt205/thgame/api/GameService.kt
@@ -14,8 +14,6 @@ import org.json.JSONObject
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
-
-
 typealias GameServiceCallback = (state:Game?, errorCode:Int? ) -> Unit
 
 object GameService {
